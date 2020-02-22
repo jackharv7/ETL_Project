2f01e470065f4237d948b2d9911602cfc12bf058
Matthew Richtmyer <mrichtmyer94@gmail.com>
2020-02-22 13:29:59 -0500

    Adding sql and python notebook for data transformations

---
+++ code/postgreSQL/sports_data.sql
@@ -0,0 +1,58 @@
+-- Delete tables if they already exist
+DROP TABLE IF EXISTS nfl;
+DROP TABLE IF EXISTS nba;
+CREATE TABLE "nfl" (
+	id serial Primary Key,
+	Team_Name varchar(100),
+	"2000" float,
+	"2001" float,
+	"2002" float,
+	"2003" float,
+	"2004" float,
+	"2005" float,
+	"2006" float,
+	"2007" float,
+	"2008" float,
+	"2009" float,
+	"2010" float,
+	"2011" float,
+	"2012" float,
+	"2013" float,
+	"2014" float,
+	"2015" float,
+	"2016" float,
+	"2017" float,
+	"2018" float,
+	"20_Yr_Avg" float,
+	US_State varchar(25)
+);
+CREATE TABLE "nba" (
+	id serial Primary Key,
+	Team_Name varchar(100),
+	"2000" float,
+	"2001" float,
+	"2002" float,
+	"2003" float,
+	"2004" float,
+	"2005" float,
+	"2006" float,
+	"2007" float,
+	"2008" float,
+	"2009" float,
+	"2010" float,
+	"2011" float,
+	"2012" float,
+	"2013" float,
+	"2014" float,
+	"2015" float,
+	"2016" float,
+	"2017" float,
+	"2018" float,
+	"20_Yr_Avg" float,
+	US_State varchar(25)
+
+
+
+ 
+
+				  
